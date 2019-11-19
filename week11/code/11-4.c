e4ab94cf751f522756b5de70f63d555fc0547e68
bzxaxjl <1278544653@qq.com>
2019-11-19 00:31:28 -0800

    week11

---
+++ week11/code/11-4.c
@@ -0,0 +1,26 @@
+#include"my.h"
+int main()
+{
+	pid_t pid;
+	int k,status;
+	if(pid<0)
+	{	
+		perror("fork failed!\n");
+		return -1;	
+	}
+	else if(pid==0)
+	{
+		printf("child %d is running\n",getpid());
+		while(1);
+		exit(120);
+	}
+	else{
+		if((k=wait(&status))!=0){
+			if(WIFEXITED(status))
+				printf("child exit with exit!\nexit cod =%d\n",WEXITSTATUS(status));	
+			else if(WIFSIGNALED(status))
+				printf("child terminstated with sinal!\n signal number = %d\n",WTERMSIG(status));	
+		}	
+		exit(0);
+	}
+}
