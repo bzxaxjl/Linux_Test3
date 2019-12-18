daed5b345002950995e02151f7a0a9c4c04c52fd
bzxaxjl <1278544653@qq.com>
2019-12-18 07:25:16 -0800

    week15-2

---
+++ week15-2/code/my.h
@@ -0,0 +1,13 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<limits.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/time.h>
+#include <sys/types.h>
+#include<semaphore.h>
+#define LOOP 10000000
+#define NUM 4
+
